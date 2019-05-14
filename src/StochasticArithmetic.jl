9345f77d6cd02fc8f0162d85b3711407ba130738
Luiz Faria <maltezfaria@gmail.com>
2019-05-14 15:45:00 +0200

    simplify exported functions

--- src/StochasticArithmetic.jl
+++ src/StochasticArithmetic.jl
@@ -2,7 +2,7 @@ module StochasticArithmetic
 
 using Statistics, Cassette
 
-export UP, DWN, RND, SFloat64, SFloat32, value, @reliable_digits
+export @reliable_digits
 
 include("EFT.jl")
 include("rounding.jl")
